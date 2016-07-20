6bde0c374a5e51c33ba029a97f62d839a8f227ef
szymon <sz6315@ic.ac.uk>
2016-07-20 15:26:05 +0200

    commit for test

--- formhero/apps/models.py
+++ formhero/apps/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 
 class App(models.Model):
     name = models.TextField()
+    host_list = ArrayField(models.CharField(max_length=100), default=[])
