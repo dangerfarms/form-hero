2bd9986de6e661a0b9dd2d2592fde3e3fc0c170a
jdw559 <jdw559@cs.bham.ac.uk>
2016-07-20 16:14:29 +0100

    Change FormEntry to FormSubmission

--- formhero/providers/dbform/__init__.py
+++ formhero/providers/dbform/__init__.py
@@ -1,5 +1,5 @@
 from formhero.providers.base import BaseBackend
-from formhero.providers.dbform.models import FormEntry
+from formhero.providers.dbform.models import FormSubmission
 
 
 class Backend(BaseBackend):
@@ -7,4 +7,4 @@ class Backend(BaseBackend):
     Save form data on form instance.
     """
     def handle_data(self, form_obj, data):
-        FormEntry.objects.create(form=form_obj, form_data=data)
+        FormSubmission.objects.create(form=form_obj, form_data=data)
