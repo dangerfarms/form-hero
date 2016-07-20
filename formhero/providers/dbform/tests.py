89a0f8c6853ed11c5aa575b7e32ad1251fa707f6
jdw559 <jdw559@cs.bham.ac.uk>
2016-07-20 14:01:31 +0100

    Add test to check form entry data is the same as form data

--- formhero/providers/dbform/tests.py
+++ formhero/providers/dbform/tests.py
@@ -7,16 +7,27 @@ from formhero.providers.dbform.models import FormEntry
 
 
 class DatabaseFormTest(TestCase):
-    def test_should_save_a_form_entry(self):
-        an_app = App.objects.create(name='this_app')
-        data = {'name': 'Dave',
+
+    def setUp(self):
+        self.an_app = App.objects.create(name='this_app')
+        self.data = {'name': 'Dave',
                 'inquirer email': 'dave@example.com',
                 'message': 'This is a test message'
                 }
-        a_form = Form.objects.create(app=an_app, name='Form name', handler='db', config=data)
+        self.a_form = Form.objects.create(app=self.an_app, name='Form name', handler='db', config=self.data)
 
-        db_backend = Backend()
-        db_backend.handle_data(a_form, a_form.config)
-        my_form_entry = FormEntry.objects.get(form=a_form)
+        self.db_backend = Backend()
 
-        self.assertEquals(my_form_entry.form, a_form)
+    def test_should_save_a_form_entry(self):
+        self.db_backend.handle_data(self.a_form, self.a_form.config)
+        my_form_entry = FormEntry.objects.get(form=self.a_form)
+
+        self.assertEqual(my_form_entry.form, self.a_form)
+
+    def test_should_check_form_entry_data_is_the_same_as_form_data(self):
+        self.db_backend.handle_data(self.a_form, self.a_form.config)
+        my_form_entry = FormEntry.objects.get(form=self.a_form)
+
+        self.assertEqual(my_form_entry.form_data['message'], self.data['message'])
+        self.assertEqual(my_form_entry.form_data['name'], self.data['name'])
+        self.assertEqual(my_form_entry.form_data['inquirer email'], self.data['inquirer email'])
