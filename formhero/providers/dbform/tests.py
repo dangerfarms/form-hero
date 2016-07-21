e131be0c065851f0f40410655c472768a5329a54
jdw559 <jdw559@cs.bham.ac.uk>
2016-07-21 10:21:58 +0100

    Add comments to db tests

--- formhero/providers/dbform/tests.py
+++ formhero/providers/dbform/tests.py
@@ -8,6 +8,12 @@ from formhero.providers.dbform.models import FormSubmission
 
 class DatabaseFormTest(TestCase):
     def setUp(self):
+        """
+        Create an app, a form, data, and a backend for the tests.
+        Variable db_config is empty as we don't need any
+        configurations (but django complains if nothing is given
+        to the config field of Form)
+        """
         self.an_app = App.objects.create(name='this_app')
         self.data = {'name': 'Dave',
                      'inquirer email': 'dave@example.com',
@@ -18,13 +24,20 @@ class DatabaseFormTest(TestCase):
 
         self.db_backend = Backend()
 
-    def test_should_save_a_form_entry(self):
+    def test_should_save_a_form_submission(self):
+        """
+        Check that FormSubmission is saved in the db.
+        """
         self.db_backend.handle_data(self.a_form, self.data)
         my_form_submission = FormSubmission.objects.get(form=self.a_form)
 
         self.assertEqual(my_form_submission.form, self.a_form)
 
     def test_should_check_form_submission_data_is_the_same_as_form_data(self):
+        """
+        Check that data saved in FormSubmission is the same as
+        original test data.
+        """
         self.db_backend.handle_data(self.a_form, self.data)
         my_form_submission = FormSubmission.objects.get(form=self.a_form)
 
